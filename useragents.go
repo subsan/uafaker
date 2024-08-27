a48a9dd4b13a7f8b12bff3f361d5c1203fda781b
subsan <subsan@online.ua>
2024-08-27 11:05:01 +0300

    update database

--- useragents.go
+++ useragents.go
@@ -3663,6 +3663,10 @@ var userAgentStorage = []userAgentStorageItem{
 	},
 	// edge/iOS
 	{
+		userAgent: "Mozilla/5.0 (iPhone; CPU iPhone OS 17_6_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 EdgiOS/128.2739.42 Mobile/15E148 Safari/605.1.15",
+		bitmap:    0b1000001000010,
+	},
+	{
 		userAgent: "Mozilla/5.0 (iPhone; CPU iPhone OS 17_6_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 EdgiOS/127.2651.111 Mobile/15E148 Safari/605.1.15",
 		bitmap:    0b1000001000010,
 	},
